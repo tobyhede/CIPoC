e18cb5e3c414f3248d568f548d29e943474f0545
Toby Hede <tobyhede@info-architects.net>
2011-07-19 20:54:15 +1000

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CIPoC::Application.config.secret_token = 'a36466a7b30399f9b49156add8340fa8b95eb835debfccc961d787a3c6d2bd9e17aab86436e1676c8715ba8031102d5f61d483d76c13f29d6788c9c5d4270a50'
